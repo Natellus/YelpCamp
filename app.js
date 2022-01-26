a4fc8ee084ca97e410fc28fc2c2b648e8c8889ea
Natalia Usacheva <natellus@yahoo.com>
2022-01-26 08:06:11 +0000

    Creating reviews

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     // useNewUrlParser: true,
@@ -93,6 +94,15 @@ app.delete('/campgrounds/:id',catchAsync(async (req, res) => {
     const campground = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
 }))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req,res) =>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
 
 app.all('*', (req,res,next)=>{
     next(new ExpressError('Page not found', 404))
