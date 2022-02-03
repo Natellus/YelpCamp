11320624faafced2a99d1b0e438e2c03902ca1a6
Natalia Usacheva <natellus@yahoo.com>
2022-02-03 17:20:46 +0000

    actually fixing register route

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,23 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const { Passport } = require('passport/lib');
+const { nextTick } = require('process');
 
 router .get('/register', (req, res)=>{
     res.render('users/register')
 });
 
-router.post('/register', catchAsync(async(req,res)=>{
+router.post('/register', catchAsync(async(req,res, next)=>{
     try{
     const {email, username,password} = req.body;
     const user = new User({email, username});
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Yelp Camp!');
-    res.redirect('/campgrounds');
+    req.login(registeredUser, err=>{
+        if(err) return nextTick(err); 
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    })
+   
     }catch(e){
         req.flash('error', e.message);
         res.redirect('register');
