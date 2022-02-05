0c978575a3c0a9d26e103df265c3ad8275be82c0
Natalia Usacheva <natellus@yahoo.com>
2022-02-05 13:05:26 +0000

    Multer middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,22 +6,47 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 
+// router.get('/', catchAsync(campgrounds.index))
 
-router.get('/', catchAsync(campgrounds.index))
+// router.get('/new',isLoggedIn, campgrounds.renderNewForm)
 
-router.get('/new',isLoggedIn, campgrounds.renderNewForm)
+// // router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+// router.post('/', upload.array('image'),(req,res)=>{
+//     console.log(req.body,req.files);
+//     res.send('It worked?');
+// })
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(upload.array('image'), (req, res) => {
+    console.log(req.body,req.files);
+    res.send('It worked?')
+    })
 
-router.get('/:id/edit', isLoggedIn,isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-router.delete('/:id',isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
+
+
+// router.get('/:id', catchAsync(campgrounds.showCampground));
+
+
+
+// router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
+
+// router.delete('/:id',isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
